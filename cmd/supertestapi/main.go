f5905c63b84f26e6baa8a32af69e5b6da75c09b7
Brutie <125909656+astabrutie8@users.noreply.github.com>
2025-12-15 00:38:01 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/supertestapi/main.go
@@ -0,0 +1,23 @@
+// cmd/supertestapi/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "supertestapi/internal/supertestapi"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := supertestapi.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
